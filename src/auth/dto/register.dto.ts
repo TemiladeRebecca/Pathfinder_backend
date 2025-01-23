3bf8a9b36413416d049db4b2f7e91d313880f3b0
johnson-oragui <johnson.oragui@gmail.com>
2025-01-24 00:42:53 +0100

    feat: added register user dto and response class dto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,36 +1,67 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  Validate,
+} from 'class-validator';
+import { uuidv7 } from 'uuidv7';
+import { ValidateRegisterUser } from './utils/utils.dto';
 
-export class RegisterUserSchema {
+export class RegisterUserDto {
+  @Validate(ValidateRegisterUser)
+  validateAllFields: boolean;
+
   @ApiProperty({
     example: 'johnson@email.com',
+    required: true,
   })
   @IsEmail()
   email: string;
 
   @ApiProperty({
-    example: 'johnson@email.com',
+    example: 'Johnson1234#',
   })
-  @IsOptional()
+  @IsNotEmpty()
   @IsString()
-  first_name: string;
-
-  @ApiProperty({
-    example: 'johnson@email.com',
-  })
-  @IsOptional()
-  @IsString()
-  last_name: string;
-
-  @ApiProperty({
-    example: 'johnson@email.com',
-  })
-  @IsOptional()
-  @IsString()
+  @IsStrongPassword()
   password: string;
-  confirm_password: string;
+
+  @ApiProperty({
+    example: 'Johnson1234#',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  confirmPassword: string;
+
+  constructor(partial: Partial<RegisterUserDto>) {
+    Object.assign(this, partial);
+  }
 }
-export class LoginUserSchema {
-  email: string;
-  password: string;
+export class RegisterUserResponse {
+  constructor(partial: Partial<RegisterUserResponse>) {
+    Object.assign(this, partial);
+  }
+
+  @ApiProperty({
+    example: 201,
+  })
+  statusCode: number;
+
+  @ApiProperty({
+    example: 'User registered successfully',
+  })
+  message: string;
+
+  @ApiProperty({
+    example: {
+      id: uuidv7(),
+      email: 'johnson@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  })
+  data: Record<string, any>;
 }
