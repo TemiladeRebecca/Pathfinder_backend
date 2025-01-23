ca2ca397e0706fd75c993987e419b84c573dcb93
johnson-oragui <johnson.oragui@gmail.com>
2025-01-24 00:41:38 +0100

    feat: added userService to userModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './user.model';
+import { UserService } from './user.service';
 
 @Module({
-  providers: [],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserService],
   controllers: [UsersController],
+  exports: [UserService],
 })
 export class UserModule {}
