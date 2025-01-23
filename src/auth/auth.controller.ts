5854474df0bd483d227be7ca47fd9c3e7d1e465e
johnson-oragui <johnson.oragui@gmail.com>
2025-01-24 00:43:45 +0100

    feat: added endpoint to create/register user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { RegisterUserDto, RegisterUserResponse } from './dto/register.dto';
+import {
+  SwaggerBadRequest,
+  SwaggerInternalserverError,
+} from 'src/utils/api-responses';
 
 @Controller('auth')
-export class AuthController {}
+@ApiTags('AUTH')
+export class AuthController {
+  constructor(private userService: UserService) {}
+
+  @Post('register')
+  @ApiOperation({
+    summary: 'Register a new User',
+  })
+  @ApiResponse({
+    example: RegisterUserResponse,
+    type: RegisterUserResponse,
+    status: 201,
+  })
+  @ApiResponse(SwaggerBadRequest)
+  @ApiResponse(SwaggerInternalserverError)
+  @HttpCode(HttpStatus.CREATED)
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async registerUser(
+    @Body() schema: RegisterUserDto,
+  ): Promise<RegisterUserResponse> {
+    try {
+      const newUser = this.userService.createUser(schema);
+      return new RegisterUserResponse({
+        statusCode: 201,
+        message: 'User Registered successfully',
+        data: {
+          email: (await newUser).email,
+          createdAt: (await newUser).createdAt,
+          updatedAt: (await newUser).updatedAt,
+          id: (await newUser)._id,
+        },
+      });
+    } catch (error) {
+      console.error('Error registering user: ', error);
+      throw error;
+    }
+  }
+}
