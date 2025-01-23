e7c6a427d48e424904b57330d6249d7cae4b89c7
johnson-oragui <johnson.oragui@gmail.com>
2025-01-24 00:41:07 +0100

    feat: added create and find user methods of userService class

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './user.model';
+import { Model } from 'mongoose';
+import { FindUserT } from './interfaces/find-user-interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createUser(criteria: Partial<User>): Promise<User | any> {
+    try {
+      criteria.createdAt = new Date();
+      criteria.updatedAt = new Date();
+      criteria.password = await User.setPassword(criteria.password);
+      const result = new this.userModel(criteria);
+      const newUser = await result.save();
+      // newUser.id = newUser._id;
+      return newUser;
+    } catch (error) {
+      console.error('Error creating User: ', error);
+      if (error.code === 11000) {
+        throw new ConflictException('Email already in use');
+      }
+
+      throw error;
+    }
+  }
+  async findUser(criteria: FindUserT): Promise<User | null> {
+    try {
+      const where = {
+        ...(criteria.id && { id: criteria.id }),
+        ...(criteria.email && { email: criteria.email }),
+      };
+      const foundUser = this.userModel.findOne(where).exec();
+      return foundUser;
+    } catch (error) {
+      console.error('Error fetching User: ', error);
+      throw error;
+    }
+  }
+}
