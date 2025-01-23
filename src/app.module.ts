d1ae2d9f0473faec9284d8ca0f21d3f7e1169ad7
johnson-oragui <johnson.oragui@gmail.com>
2025-01-24 00:39:29 +0100

    feat: added mongoose setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
+@Global()
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const connectionString: string =
+          configService.get<string>('DATABASE_URI');
+        return {
+          uri: connectionString,
+          maxPoolSize: 5,
+        };
+      },
+      inject: [ConfigService],
+      imports: [ConfigModule],
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
