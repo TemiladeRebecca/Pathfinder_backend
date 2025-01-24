47e4f8359e498020fe501d1d2b85a27d95b815f1
johnson-oragui <johnson-oragui@gmail.com>
2025-01-24 19:52:14 +0100

    feat: updated userModule with user-session model forfeature

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,14 @@ import { UsersController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.model';
 import { UserService } from './user.service';
+import { UserSession, UserSessionSchema } from './user-session.model';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: UserSession.name, schema: UserSessionSchema },
+    ]),
   ],
   providers: [UserService],
   controllers: [UsersController],
